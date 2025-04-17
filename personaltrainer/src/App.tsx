3788948ff46203c9a23cf59f25dc8f67c34ccc77
janne <j_n_812@hotmail.com>
2025-04-17 15:32:06 +0300

    deleted unused imports from App.tsx

--- personaltrainer/src/App.tsx
+++ personaltrainer/src/App.tsx
@@ -1,8 +1,6 @@
 import { AppBar, Box, Container, Toolbar, Typography } from "@mui/material";
 import "./App.css";
 import AppTabs from "./AppTabs";
-import AppLinks from "./AppLinks";
-import { Outlet } from "react-router";
 
 function App() {
   return (
