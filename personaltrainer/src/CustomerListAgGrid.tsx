ea0a2e849599ddcc4a62d131b917c381054c8521
janne <j_n_812@hotmail.com>
2025-04-23 05:23:47 +0300

    added delete customer functionality and button

--- personaltrainer/src/CustomerListAgGrid.tsx
+++ personaltrainer/src/CustomerListAgGrid.tsx
@@ -3,8 +3,9 @@ import { Customer } from "./Types";
 
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-theme-quartz.css";
-import { ColDef } from "ag-grid-community";
+import { ColDef, ICellRendererParams } from "ag-grid-community";
 import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+import Button from "@mui/material/Button";
 ModuleRegistry.registerModules([AllCommunityModule]);
 
 // Setting base url
@@ -38,36 +39,68 @@ export default function CustomerListAgGrid() {
     { field: "city", cellStyle: { textAlign: "start" } },
     { field: "phone", cellStyle: { textAlign: "start" } },
     { field: "email", cellStyle: { textAlign: "start" } },
+    {
+      sortable: false,
+      filter: false,
+      flex: 0.5,
+      field: "_links.self.href",
+      headerName: "",
+      cellRenderer: (params: ICellRendererParams) => (
+        <Button
+          color="secondary"
+          size="small"
+          onClick={() => handleDelete(params.value)}>
+          DELETE
+        </Button>
+      ),
+    },
   ]);
 
   // Get customer data and add customer id to data
-  useEffect(() => {
-    const fetchCustomers = async () => {
-      try {
-        const response = await fetch(`${BASE_URL}/customers`);
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        const customerWID: Customer[] = data._embedded.customers.map(
-          (customer: Customer) => {
-            const id: String = customer._links.self.href.split("/").pop() || "";
-            return {
-              ...customer,
-              id,
-            };
-          }
-        );
-        setCustomers(customerWID);
-      } catch (err: any) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
 
+  const fetchCustomers = async () => {
+    try {
+      const response = await fetch(`${BASE_URL}/customers`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      const customerWID: Customer[] = data._embedded.customers.map(
+        (customer: Customer) => {
+          const id: String = customer._links.self.href.split("/").pop() || "";
+          return {
+            ...customer,
+            id,
+          };
+        }
+      );
+      setCustomers(customerWID);
+    } catch (err: any) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
     fetchCustomers();
   }, []);
+
+  const handleDelete = (url: string) => {
+    if (window.confirm("Are you sure you want to delete car?")) {
+      deleteCustomer(url);
+    }
+  };
+
+  const deleteCustomer = (url: string) => {
+    const options = {
+      method: "DELETE",
+    };
+
+    fetch(url, options)
+      .then(() => fetchCustomers())
+      .catch((err) => console.error(err));
+  };
 
   if (loading) {
     return <div>Loading...</div>;
