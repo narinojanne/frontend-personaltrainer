704a3ce821ffbf680f808f6a795257fc4c059afa
janne <j_n_812@hotmail.com>
2025-04-27 19:13:49 +0300

    added deployment settings

--- personaltrainer/src/main.tsx
+++ personaltrainer/src/main.tsx
@@ -2,38 +2,43 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createHashRouter, RouterProvider } from "react-router";
 import Home from "./Home.tsx";
 import Error from "./Error.tsx";
 import TrainingListAgGrid from "./TrainingListAgGrid.tsx";
 import CustomerListAgGrid from "./CustomerListAgGrid.tsx";
 import TrainingsCalendar from "./TrainingsCalendar.tsx";
 
-const router = createBrowserRouter([
+const router = createHashRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <Error />,
+      children: [
+        {
+          element: <Home />,
+          index: true,
+        },
+        {
+          path: "customerlistaggrid",
+          element: <CustomerListAgGrid />,
+        },
+        {
+          path: "traininglistaggrid",
+          element: <TrainingListAgGrid />,
+        },
+        {
+          path: "trainingscalendar",
+          element: <TrainingsCalendar />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    errorElement: <Error />,
-    children: [
-      {
-        element: <Home />,
-        index: true,
-      },
-      {
-        path: "customerlistaggrid",
-        element: <CustomerListAgGrid />,
-      },
-      {
-        path: "traininglistaggrid",
-        element: <TrainingListAgGrid />,
-      },
-      {
-        path: "trainingscalendar",
-        element: <TrainingsCalendar />,
-      },
-    ],
-  },
-]);
+    basename: import.meta.env.DEV ? "/" : import.meta.env.BASE_URL,
+  }
+);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
