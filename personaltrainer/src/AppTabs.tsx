f36bf9224aaead4d821463160da1d3471d70486c
janne <j_n_812@hotmail.com>
2025-04-27 15:38:12 +0300

    calendar for trainings started

--- personaltrainer/src/AppTabs.tsx
+++ personaltrainer/src/AppTabs.tsx
@@ -4,6 +4,7 @@ import { TabContext, TabList, TabPanel } from "@mui/lab";
 import Home from "./Home";
 import CustomerListAgGrid from "./CustomerListAgGrid";
 import TrainingListAgGrid from "./TrainingListAgGrid";
+import TrainingsCalendar from "./TrainingsCalendar";
 
 export default function AppTabs() {
   const [value, setValue] = useState("1");
@@ -24,6 +25,7 @@ export default function AppTabs() {
             <Tab label="Home" value="1" />
             <Tab label="Customers" value="2" />
             <Tab label="Trainings" value="3" />
+            <Tab label="Calendar" value="4" />
           </TabList>
         </Box>
         <TabPanel value="1" sx={{ p: 0 }}>
@@ -39,6 +41,11 @@ export default function AppTabs() {
         <TabPanel value="3" sx={{ p: 0 }}>
           <Box sx={{ height: "80vh", width: "100%" }}>
             <TrainingListAgGrid />
+          </Box>
+        </TabPanel>
+        <TabPanel value="4" sx={{ p: 0 }}>
+          <Box sx={{ height: "80vh", width: "100%" }}>
+            <TrainingsCalendar />
           </Box>
         </TabPanel>
       </TabContext>
---
+++ personaltrainer/src/TrainingsCalendar.tsx
@@ -0,0 +1,64 @@
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import { useEffect, useState } from "react";
+import { Training } from "./Types";
+
+// Setting base url
+const BASE_URL =
+  "https://customer-rest-service-frontend-personaltrainer.2.rahtiapp.fi/api";
+
+export default function TrainingsCalendar() {
+  const [trainings, setTrainings] = useState<Training[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  // Get trainings data
+  const fetchTrainings = async () => {
+    try {
+      const response = await fetch(`${BASE_URL}/gettrainings`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      setTrainings(data);
+    } catch (err: any) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchTrainings();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  const events = trainings.map((training) => ({
+    title: `${training.activity} / ${training.customer?.firstname} ${training.customer?.lastname}`,
+    date: training.date,
+  }));
+
+  return (
+    <div style={{ margin: 20 }}>
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin]}
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek,timeGridDay",
+        }}
+        initialView="dayGridMonth"
+        height="auto"
+        events={events}
+      />
+    </div>
+  );
+}
